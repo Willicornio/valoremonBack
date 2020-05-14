c0f91a6b5018efd2ab7b06a4d392e773c2a7a8e9
Guillermo Manterola Rojas <guillermo.manterola@raona.com>
2020-05-14 16:39:15 +0200

    2

--- index.js
+++ index.js
@@ -27,40 +27,81 @@ app.listen(app.get('port'), () => {
 
     console.log(`Server on port ${app.get('port')}`);
 
-    var options = {
-        mode: 'text',
-        //  pythonPath: 'C:/Users/GuillermoManterolaRo/Miniconda3/python.exe',
-        pythonOptions: ['-u'],
-        scriptPath: '../bigadtatrabajo',
-           args: ['Hey ', 'mabuel']
-        //args: ['Dragon,Ghost', 'Pressure,Telepathy',150,100,120,100,120,90]
-      };
+ 
 
-
-    PythonShell.PythonShell.run('prueba.py', options, function (err, results) {
-        if (err) 
-          throw err;
-        // Results is an array consisting of messages collected during execution
-        console.log('results: %j', results);
-      });
-
-    // const spawn = require("child_process").spawn;
-
-
-    // // const process = spawn("python", ["../bigadtatrabajo/cliente.py", 'Dragon,Ghost', 'Pressure,Telepathy',150,100,120,100,120,90])
-    
-    // const process = spawn("python", ["../bigadtatrabajo/prueba.py", 'Dragon,Ghost', 'Pressure,Telepathy'])
-
-    
-    //     process.stdout.on("data", data =>{
-    //         console.log(data.toString());
-    //        resolve(data.toString()); // <------------ by default converts to utf-8
-  
+    arjejadeje();
    
    
 })
 
+function arjejadeje(req, res){
+  var options = {
+    mode: 'text',
+      pythonPath: 'C:/Users/GuillermoManterolaRo/Miniconda3/python.exe',
+    pythonOptions: ['-u'],
+    scriptPath: '../bigadtatrabajo',
+       args: ["joblib","sklearn","pandas"]
+    //args: ['Dragon,Ghost', 'Pressure,Telepathy',150,100,120,100,120,90]
+  };
 
+
+PythonShell.PythonShell.run('prueba.py', options, function (err, results) {
+    if (err) 
+      throw err;
+    // Results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
+  });
+}
+
+function callD_alembert(req, res) {
+  // using spawn instead of exec, prefer a stream over a buffer
+  // to avoid maxBuffer issue
+  console.log("entro");
+  var spawn = require('child_process').spawn;
+  var process = spawn('python', ['../bigadtatrabajo/cliente.py',
+  'Dragon,Ghost', 'Pressure,Telepathy',150,100,120,100,120,90
+  ]);
+  console.log(process);
+  process.stdout.on('stdout', function (data) {
+    console.log(data);
+  });
+  process.stdout.on('end', function() {
+   console.log("acabé");
+});
+}
+
+
+function run() {
+  const { spawn } = require('child_process');
+
+  const process = spawn('python', ['../bigadtatrabajo/cliente.py',"Hey","Manul","joblib"]);
+  process.stdout.on(
+    'data',
+    (data) => console.log(data.toString())
+  );
+}
+(() => {
+  try {
+    run()
+    // process.exit(0)
+  } catch (e) {
+    console.error(e.stack);
+    process.exit(1);
+  }
+})();
+
+function asereje(req, res) {
+  const { spawn } = require('child_process');
+const pyProg = spawn('python', ['../bigadtatrabajo/cliente.py',"Hey","Manul","joblib"]);
+
+pyProg.stdout.on('data', function(data) {
+
+    console.log(data.toString());  
+});
+pyProg.stderr.on('data', (data) => {
+  console.log('Error-->'+ data);
+});
+}
 //     return new Promise((resolve, reject) => {
 
 //         try {
@@ -104,3 +145,4 @@ app.listen(app.get('port'), () => {
 //           #                     "Special Defense":[120],
 //            #                    "Speed":[90]
 //             #                   });
+
