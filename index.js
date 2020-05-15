22c784f1c363e52ad284dfae6aa74c3ed870df53
Guillermo Manterola Rojas <guillermo.manterola@raona.com>
2020-05-15 17:49:44 +0200

    acabado

--- index.js
+++ index.js
@@ -29,120 +29,35 @@ app.listen(app.get('port'), () => {
 
  
 
-    arjejadeje();
+    // Tier();
    
    
 })
 
-function arjejadeje(req, res){
+
+
+app.post('/gettier', async (req, res) =>{
+
+  console.log("hola! :")
+  console.log(req.body);
   var options = {
     mode: 'text',
       pythonPath: 'C:/Users/GuillermoManterolaRo/Miniconda3/python.exe',
     pythonOptions: ['-u'],
     scriptPath: '../bigadtatrabajo',
-       args: ["joblib","sklearn","pandas"]
-    //args: ['Dragon,Ghost', 'Pressure,Telepathy',150,100,120,100,120,90]
+      // args: ["joblib","sklearn","pandas"]
+    // args: ['Dragon,Ghost', 'Pressure,Telepathy',150,100,120,100,120,90]
+        args: [req.body.type, req.body.Abilities, req.body.Hp, req.body.Attack, req.body.Defense, req.body.SpecialAttack, req.body.SpecialDefense, req.body.Speed]
+
+ 
   };
 
 
-PythonShell.PythonShell.run('prueba.py', options, function (err, results) {
+PythonShell.PythonShell.run('cliente.py', options, function (err, results) {
     if (err) 
       throw err;
     // Results is an array consisting of messages collected during execution
-    console.log('results: %j', results);
+    //return(results);
+    res.status(200).json(results)
   });
-}
-
-function callD_alembert(req, res) {
-  // using spawn instead of exec, prefer a stream over a buffer
-  // to avoid maxBuffer issue
-  console.log("entro");
-  var spawn = require('child_process').spawn;
-  var process = spawn('python', ['../bigadtatrabajo/cliente.py',
-  'Dragon,Ghost', 'Pressure,Telepathy',150,100,120,100,120,90
-  ]);
-  console.log(process);
-  process.stdout.on('stdout', function (data) {
-    console.log(data);
-  });
-  process.stdout.on('end', function() {
-   console.log("acabé");
 });
-}
-
-
-function run() {
-  const { spawn } = require('child_process');
-
-  const process = spawn('python', ['../bigadtatrabajo/cliente.py',"Hey","Manul","joblib"]);
-  process.stdout.on(
-    'data',
-    (data) => console.log(data.toString())
-  );
-}
-(() => {
-  try {
-    run()
-    // process.exit(0)
-  } catch (e) {
-    console.error(e.stack);
-    process.exit(1);
-  }
-})();
-
-function asereje(req, res) {
-  const { spawn } = require('child_process');
-const pyProg = spawn('python', ['../bigadtatrabajo/cliente.py',"Hey","Manul","joblib"]);
-
-pyProg.stdout.on('data', function(data) {
-
-    console.log(data.toString());  
-});
-pyProg.stderr.on('data', (data) => {
-  console.log('Error-->'+ data);
-});
-}
-//     return new Promise((resolve, reject) => {
-
-//         try {
-//             if (_.isEmpty(regressionModel)) {
-//                 console.log('calling python');
-//                 regressionModel = pynode.call('predictPokemon','Dragon,Ghost','Pressure,Telepathy',150,100,120,100,120,90);
-//             }
-//             resolve(regressionModel);
-//         } catch (err) {
-//             console.log(err);
-//             reject('failed to load housing variables');
-//         }
-//     })
-//         .then(response => res.send(response))
-//         .catch(err => res.err(err));
-// })
-
-
-// // return new Promise((resolve, reject) =>{
-//      process.stdout.on("data", data =>{
-//          console.log(data.toString());
-// //         resolve(data.toString()); // <------------ by default converts to utf-8
-
-// //     })
-// //     process.stderr.on("data", reject)
-// // })
-
-
-
-
-
-//   #pokemonAMeter = pd.DataFrame({"Types":['Dragon,Ghost'],
-//   #                             "Abilities":["Pressure,Telepathy"],
-//    #                            "Tier":["No"],
-//    ##                            "Tier":["No"],
-//    ###                            "Tier":["No"],
-//       #                         "HP":[150],
-//        #                        "Attack":[100],
-//         #                       "Defense":[120],
-//          #                      "Special Attack":[100],
-//           #                     "Special Defense":[120],
-//            #                    "Speed":[90]
-//             #                   });
-
